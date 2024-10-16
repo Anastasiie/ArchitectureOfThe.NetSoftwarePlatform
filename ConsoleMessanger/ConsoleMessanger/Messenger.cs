6ffb97348e4a77c4aff3e4550c4433604a7a0c9f
Asya <bkysnoe@gmail.com>
2024-10-16 19:43:56 +0300

    Initial commit

---
+++ ConsoleMessanger/ConsoleMessanger/Messenger.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleMessanger
+{
+    public class Messenger
+    {
+        public List<Message> Messages = new List<Message>();
+        public void SendMessage(User sender, string text)
+        {
+            Messages.Add(new Message(text, sender));
+        }
+        public void ShowMessages()
+        {
+            foreach (var message in Messages)
+            {
+                Console.WriteLine($"{message.Sender.Username}: {message.Text}");
+            }
+        }
+    }
+
+}
---
+++ ConsoleMessanger/ConsoleMessanger/Program.cs
@@ -0,0 +1,16 @@
+﻿using ConsoleMessanger;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        User user1 = new User("Alice");
+        User user2 = new User("Bob");
+        Messenger messenger = new Messenger();
+
+        messenger.SendMessage(user1, "Hi, Bob!");
+        messenger.SendMessage(user2, "Hello, Alice!");
+
+        messenger.ShowMessages();
+    }
+}
